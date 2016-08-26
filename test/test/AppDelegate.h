03bee376bffc6524d99c121e641c4097fbd8a180
Your Name <you@example.com>
2016-08-26 16:17:56 +0800

    初始化项目

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by etcxm on 2016/8/26.
+//  Copyright © 2016年 etc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
